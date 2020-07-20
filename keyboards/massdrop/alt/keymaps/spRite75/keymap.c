3750652229747c042e28b8c29a2fac71e03e84c5
Luke Montgomery <luke.montgomery@hotmail.com>
2020-07-20 12:18:13 +1000

    broken: deciding to switch to RGB matrix instead i think

--- keyboards/massdrop/alt/keymaps/spRite75/keymap.c
+++ keyboards/massdrop/alt/keymaps/spRite75/keymap.c
@@ -592,7 +592,7 @@ led_instruction_t led_instructions[] = {
     { .layer = LO_SETTINGS, .id0 = 16, .r = 0, .g = 255, .b = 0, .dLayer = 3, .flags = LED_FLAG_MATCH_ID | LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_DEFAULT_LAYER | LED_FLAG_USE_RGB },
     /* -- end -- */
     /* end */
-    // (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK))
+
     { .condition_func = spRite75_capslock_enabled, .id0 = 1073741824, .flags = LED_FLAG_MATCH_ID | LED_FLAG_CONDITION | LED_FLAG_USE_RGB },
 
     //end must be set to 1 to indicate end of instruction set
